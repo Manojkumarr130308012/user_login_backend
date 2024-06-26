8f043fb4964af634e5273dc4d5ab2e7fd57c9c56
manojkumar <manojkumarr21@gmail.com>
2024-06-26 18:21:19 +0530

    testing

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -8,5 +10,5 @@ const config=require('./config/config.json');
 
 const middleware=require('./middleware/middleware')
 app.use(middleware);
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(port, () => console.log(`url-shortener listening on port ${config.app.port}!`));
